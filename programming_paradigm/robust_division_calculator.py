78fc4894e6d506e9fb4e05e9ddf7a089a2d352a9
elijahudi <elijahudi@gmail.com>
2024-10-14 15:55:21 -0600

    edited commit for robust division calculator with command line arguments

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,6 +1,6 @@
-def safe_dvide(numerator, denominator):
+def safe_divide(numerator, denominator):
     try:
-        numberator = float(numerator)
+        numerator = float(numerator)
         denominator = float(denominator)
         result = numerator / denominator
         return f"The result of the division is {result:.1f}"
